acaef867967276cd4e4393838c9b341e7134b9a3
dangrous <5487802+dangrous@users.noreply.github.com>
2022-03-25 19:18:20 -0700

    merge sort

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -7,13 +7,34 @@
 */
 
 const mergeSort = (nums) => {
-  // code goes here
-};
+  if (nums.length <= 1) {
+    return nums
+  } else {
+    const half = Math.floor(nums.length / 2)
+    const left = nums.slice(0, half)
+    const right = nums.slice(half)
+    const result = merge(mergeSort(left), mergeSort(right))
+    return result
+  }
+}
+
+const merge = (nums1, nums2) => {
+  const result = []
+  while (nums1.length && nums2.length) {
+    if (nums1[0] < nums2[0]) {
+      result.push(nums1.shift())
+    } else {
+      result.push(nums2.shift())
+    }
+  }
+
+  return result.concat(nums1, nums2)
+}
 
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
-  const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
-  const ans = mergeSort(nums);
-  expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-});
+test('merge sort', function () {
+  const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1]
+  const ans = mergeSort(nums)
+  expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+})
